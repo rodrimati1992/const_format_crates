58d28c7dde2f9dc6909b5ed293b57b9a3fb17068
rodrimati1992 <rodrimatt1985@gmail.com>
2020-08-30 05:20:00 -0300

    Documented the ConstDebug derive macro.

--- const_format_proc_macros/src/derive_debug.rs
+++ const_format_proc_macros/src/derive_debug.rs
@@ -96,7 +96,7 @@ pub(crate) fn derive_constdebug_impl(input: DeriveInput) -> Result<TokenStream2,
                 );
 
                 field_ts.append_all(match &how_to_fmt {
-                    HowToFmt::Decimal => coerce_and_fmt(&cratep, f),
+                    HowToFmt::Regular => coerce_and_fmt(&cratep, f),
                     HowToFmt::Ignore => unreachable!(),
                     HowToFmt::Slice => fmt_slice(&cratep, f),
                     HowToFmt::Option_ => fmt_option(&cratep, f),
