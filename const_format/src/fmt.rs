5e7e7ff9b157298533d351473aff939e8927e3ea
rodrimati1992 <rodrimatt1985@gmail.com>
2021-03-27 04:01:33 -0300

    Fixed mutability of self in fmt doc example.
    Bumped version to 0.2.14

--- const_format/src/fmt.rs
+++ const_format/src/fmt.rs
@@ -187,7 +187,7 @@
 //!     impl[U,] Foo<u32, U>;
 //!     impl[U,] Foo<&str, U>;
 //!
-//!     pub const fn const_debug_fmt(&mut self, f: &mut Formatter<'_>) -> Result<(), Error> {
+//!     pub const fn const_debug_fmt(&self, f: &mut Formatter<'_>) -> Result<(), Error> {
 //!         let mut f = f.debug_struct("Foo");
 //!
 //!         // PWrapper is a wrapper for std types, which defines the formatter methods for them.
